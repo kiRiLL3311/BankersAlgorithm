41c2e34d81490c89bcd294f4e9c4fc90e0612586
Kirill Sinitsyn <1632113@mail.ru>
2026-05-23 02:05:08 +0300

    Store/calulate implementation

---
+++ src/BankersAlgorithm.java
@@ -0,0 +1,74 @@
+public class BankersAlgorithm {
+    /**
+     * Total number of processes
+     */
+    private final int processes = 3;
+
+    /**
+     * Total number of resource types
+     */
+    private final int resources = 3;
+
+    /**
+     * Maxi demanded matrix
+     */
+    private final int[][] max = {
+            {6, 4, 3},
+            {2, 2, 2},
+            {8, 1, 3}
+    };
+
+    /**
+     * Allocation matrix
+     */
+    private final int[][] allocation = {
+            {1, 2, 1},
+            {1, 0, 0},
+            {4, 0, 2}
+    };
+
+    /**
+     * Need matrix
+     */
+    private final int[][] need = new int[processes][resources];
+
+    /**
+     * Calculates the need matrix
+     *
+     * Formula:
+     * Need = Max - Allocation
+     */
+    public void calculateNeedMatrix() {
+
+        for (int i = 0; i < processes; i++) {
+
+            for (int j = 0; j < resources; j++) {
+
+                assert allocation[i][j] <= max[i][j]
+                        : "Allocation cannot reach maximum demand";
+
+                need[i][j] = max[i][j] - allocation[i][j];
+            }
+        }
+    }
+
+    /**
+     * Prints the need matrix
+     */
+    public void printNeedMatrix() {
+
+        System.out.println("Need Matrix:");
+
+        for (int i = 0; i < processes; i++) {
+
+            System.out.print("P" + i + ": ");
+
+            for (int j = 0; j < resources; j++) {
+
+                System.out.print(need[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int processes = 5;
-        int resources = 3;
+        BankersAlgorithm bankersAlgorithm = new BankersAlgorithm();
 
-        int[] available = {3, 3, 2};
+        bankersAlgorithm.calculateNeedMatrix();
 
-        int[][] max = {
-                {6, 4, 3},
-                {2, 2, 2},
-                {8, 1, 3}
-        };
-
-        int[][] allocation = {
-                {1, 2, 1},
-                {1, 0, 0},
-                {4, 0, 2}
-        };
-
-        System.out.println("Data loaded successfully.");
+        bankersAlgorithm.printNeedMatrix();
 
     }
 }
